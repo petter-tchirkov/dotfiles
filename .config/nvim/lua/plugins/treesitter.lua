2e64e8edd5a48bf2934610ccd1df9efae5ff462f
Chirkov <tchirkov.petter@gmail.com>
2025-05-17 23:13:14 +0300

    add nvim + fastfetch

---
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,13 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
+		configs.setup({
+			ensure_installed = { "html", "css", "lua", "typescript", "tsx", "vue", "json" },
+			highlight = {
+				enable = true,
+			},
+		})
+	end,
+}
